5408dc7027d37166c3427fe4ec309d7e973e3f46
egmsft <egallardo@microsoft.com>
2025-03-18 09:22:23 -0500

    adding template for outbound dns

---
+++ upgrade/imports/import_outbound.tf
@@ -0,0 +1,19 @@
+import {
+  to = azurerm_private_dns_resolver[0]
+  id = ""
+}
+
+import{
+    to = azurm_private_dns_resolver_dns_forwarding_rulest.forwarding_ruleset[0]
+    id = ""
+}
+
+import {
+    to = azurerm_private_dns_resolver_outbound_endpoint.resolver_outbound_endpoint[0]
+    id = ""
+}
+
+import {
+    to = azurerm_private_dns_resolver_virtual_network_link.resolver_vnet_link[0]
+    id = ""
+}
