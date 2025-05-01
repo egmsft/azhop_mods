d949c6b720684247a90018e433c6d430206d680e
egmsft <egallardo@microsoft.com>
2025-05-01 13:01:53 -0500

    sample for creating a custom imaged based on an azhop image

---
+++ upgrade/custom-images/scripts/ubuntu/openpbs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo " ********************************************************************************** "
+echo " *                                                                                * "
+echo " *     PBS PRO                                                                    * "
+echo " *                                                                                * "
+echo " ********************************************************************************** "
+
+cd /mnt
+apt update
+
+apt install libpq5 -y
+wget -q https://vcdn.altair.com/rl/OpenPBS/openpbs_22.05.11.ubuntu_20.04.zip
+unzip -o openpbs_22.05.11.ubuntu_20.04.zip
+dpkg -i --force-overwrite --force-confnew ./openpbs_22.05.11.ubuntu_20.04/openpbs-execution_22.05.11-1_amd64.deb
+rm -rf openpbs_22.05.11.ubuntu_20.04
+rm -f openpbs_22.05.11.ubuntu_20.04.zip
---
+++ upgrade/custom-images/scripts/ubuntu/zz-desktop-custom.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# This file is a placeholder to add all your customizations for desktop nodes
+
+apt upgrade -y
+
+apt install linux-headers-generic -y 
+apt install ubuntu-drivers-common -y 
+ubuntu-drivers autoinstall 
+
+apt-get install install -y qt-devel
+apt-get install install: -y telnet
+apt-get install -y qt5-qtsvg-devel
+apt-get install -y freeglut-devel
+apt-get install -y glibc.i686
+apt-get install -y libgcc-4.8.5-44.el7.i686
+apt-get install -y libXtst
+apt-get install -y libXmu
+apt-get install -y libXaw
+apt-get install -y ksh
+apt-get install -y screen
+apt-get install -y meld
+apt-get install -y gedit
+apt-get install -y s3geo
+apt-get install -y compat-libgfortran-41
+apt-get install -y dos2unix
+apt-get install -y ImageMagick ImageMagick-devel ImageMagick-perl
+apt-get install -y xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-fonts-ISO8859-1-75dpi
+apt-get install -y shotwell
+apt-get install -y evince
+apt-get install -y libreoffice
+apt-get install -y libgfortran.so.4
+apt-get install -y wine
+apt-get install -y libffi*
+apt-get install -y telnet
+apt-get install -y vim
+
+
+#ubuntu general packages for ansys tools
+apt-get install -y libasound2 libatk-bridge2.0-0 libatk1.0-0 libatspi2.0-0 libavahi-client3 libavahi-common3 libblkid1 libbsd0 libc6 libcairo-gobject2 libcairo2 libcom-err2 libcups2 libdatrie1 libdbus-1-3 libdrm2 libepoxy0 libexpat1 libffi7 libfontconfig1 libfreetype6 libfribidi0 libgbm1 libgcc-s1 libgcrypt20 libgdk-pixbuf2.0-0 libglib2.0-0 libgmp10 libgnutls30 libgpg-error0 libgraphite2-3 libgssapi-krb5-2 libgtk-3-0 libharfbuzz0b libhogweed5 libice6 libidn2-0 libk5crypto3 libkeyutils1 libkrb5-3 libkrb5support0 liblz4-1 liblzma5 libmount1 libnettle7 libnspr4 libnss3 libp11-kit0 libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 libpcre2-8-0 libpcre3 libpixman-1-0 libpng16-16 libselinux1 libsm6 libsqlite3-0 libsystemd0 libtasn1-6 libthai0 libunistring2 libuuid1 libwayland-client0 libwayland-cursor0 libwayland-egl1 libwayland-server0 libx11-6 libx11-xcb1 libxau6 libxcb-dri3-0 libxcb-render0 libxcb-shm0 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxdmcp6 libxext6 libxfixes3 libxft2 libxi6 libxinerama1 libxkbcommon0 libxrandr2 libxrender1 libxss1 libxtst6 lsb-core libdb1-compat libcrypt1
